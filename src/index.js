787444c83678c62e36819106eb47b35d1cb033dd
miarantala <Mia.Rantala@metropolia.fi>
2025-01-29 22:28:08 +0200

    created users.js and items.js files and added endpoints

--- src/index.js
+++ src/index.js
@@ -1,72 +1,74 @@
 import express from 'express';
-import { getItems, addItem, getItemById } from './items.js';
+import {addItem, deleteItem, editItem, getItemById, getItems} from './items.js';
+import {addUser, getUsers, login} from './users.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-// Static HTML-page in the server root
+// Static HTML-page in the serever root
 app.use('/', express.static('public'));
-
-//Middleware that reads json data from POST request body
+// Middleware that reads json data from POST-requests body
 app.use(express.json());
-
-//REST-apis under /api/-route
-app.get('/api', (req, res) => {
-  console.log('get-pyyntö juureen');
-  //res.send('Welcome to my REST API!');
-  res.json({message: 'Hi again'});
+// Resources for REST API under api-path
+app.get('/api/', (req, res) => {
+  console.log('get-pyyntö apin juureen havaittu');
+  console.log(req.url);
+  res.send('Welcome to my REST API!');
 });
 
-// Items resource endpoints
+// Endponts for Items resources
 app.get('/api/items', getItems);
 app.get('/api/items/:id', getItemById);
 app.post('/api/items', addItem);
+app.put('/api/items/:id', editItem);
+app.delete('/api/items/:id', deleteItem);
 
-//Route params
+// Endpoints for Users resources
+app.get('/api/users', getUsers);
+app.post('/api/users', addUser);
+app.post('/api/users/login', login);
+
+
+// The following code is only for demonstration
+// Route params
 app.get('/api/sum/:num1/:num2', (req, res) => {
   console.log(req.params);
   const num1 = Number(req.params.num1);
   const num2 = Number(req.params.num2);
-  // Testing if both numbers are numbers
-  // If not, send status code message
+  // Testing if one of the given parameters is not a number
+  // Error message in json
   if(isNaN(num1) || isNaN(num2)) {
     res.status(400);
-    res.json({error: 'Both parameters must be numbers'
+    res.json({
+      error: 'Both parameters must be numbers!'
     });
     return;
   }
-  res.json({sum: num1 + num2});
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2
+  });
 });
 
-//Query params
+// Query params
 app.get('/api/sum/', (req, res) => {
   console.log(req.query);
   const num1 = parseInt(req.query.num1);
   const num2 = parseInt(req.query.num2);
-  res.json({sum: num1 + num2});
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2
+  });
 });
 
-//POST body request and reading data
-app.post('/api/greet', (req, res) => {
+// POST request and data reading from body
+app.post('/api/moro', (req, res) => {
   console.log(req.body);
   res.status(200);
-  res.json({reply: 'well howdy ' + req.body.sender});
+  res.json({reply: 'no Moro ' + req.body.sender});
 });
-
-//TODO add own route and functionality
-//Count square  number of the given number
-//Error message if the given parameter is not a number
-app.get('/api/square/:nom', (req, res) => {
-  console.log(req.params);
-  const nom = Number(req.params.nom);
-  if(isNaN(nom)) {
-    res.status(400);
-    res.json({error: 'Please give a number'
-    });
-    return;
-  }
-  res.json({square: nom ** 2});
-})
 
 
 app.listen(port, hostname, () => {
--- src/items.js
+++ src/items.js
@@ -1,45 +1,72 @@
 // Mock data
 const items = [
-  { id: 1, name: "Item1" },
-  { id: 2, name: "Item2" },
-  { id: 3, name: "Item3" },
+  {id: 1, name: 'Omena'},
+  {id: 2, name: 'Appelsiini'},
+  {id: 3, name: 'Porkkana'},
+  {id: 4, name: 'Mandariini'},
 ];
 
-// Get alla items
+
+// Get all items
 const getItems = (req, res) => {
   res.json(items);
 };
 
-// Getting item by using id
+// Get an item by id
 const getItemById = (req, res) => {
   console.log('getItemById', req.params.id);
-  const item = items.find(item => item.id == req.params.id);
-  // If item is found
+  const item = items.find((item) => item.id == req.params.id);
+  console.log('item found:', item)
+  // If item found, value not undefined
   if (item) {
     res.json(item);
   } else {
-    res.status(404).json({message: 'Item not found'});
-  };
+    res.status(404).json({message: "Item not found"});
+  }
 };
 
-// Adding a new item
+// Adding an item
 const addItem = (req, res) => {
   console.log('addItem request body', req.body);
-  // If request has 'name', new item will be added to items-list
+  // If the request consist of 'name', add new item to table
   if (req.body.name) {
-    // Generate id-number to new user (one bigger than the previous)
+    // Generate a new id number to the new item (grater than the previous one)
     const latestId = items[items.length-1].id
-    // New item created and added to items-list
+    // Create a new item and add it to the items-table
     const newItem = {id: latestId + 1, name: req.body.name};
     items.push(newItem);
     res.status(201);
-    return res.json({message: 'Item added '});
+    return res.json({message: 'Item added.'});
   }
   res.status(400);
-  return res.json({message: 'Request is missing name property'});
+  return res.json({message: 'Request is missing name property.'});
 };
 
-//TODO: post, put, delete
-//TODO: add user.js, week material 2
+// Modifying an item based on id
+const editItem = (req, res) => {
+  console.log('editItem request body', req.body);
+  const item = items.find((item) => item.id == req.params.id);
+  if (item) {
+    item.name = req.body.name;
+    res.json({message: 'Item updated.'});
+  } else {
+    res.status(404).json({message: "Item not found"});
+  }
+};
 
-export {getItems, getItemById, addItem};
+// Deleting an item based on id
+const deleteItem = (req, res) => {
+  console.log('deleteItem', req.params.id);
+  const index = items.findIndex((item) => item.id == req.params.id);
+  //console.log('index', index);
+  // findIndex returns -1 if item is not found
+  if (index !== -1) {
+    // remove one item from array based on index
+    items.splice(index, 1);
+    res.json({message: 'Item deleted.'});
+  } else {
+    res.status(404).json({message: "Item not found"});
+  }
+}
+
+export {getItems, getItemById, addItem, editItem, deleteItem};
